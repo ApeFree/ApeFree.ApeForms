9646e907db4ae94edc965ffb2e02de1143fef05d
Guijie Lee <landriesnidis@yeah.net>
2022-09-04 23:02:43 +0800

    增加DialogFactory创建ApeFormsDialogProvider的扩展

---
+++ ApeFree.ApeForms.Forms/Extensions/DialogFactoryExtension.cs
@@ -0,0 +1,18 @@
+﻿using ApeFree.ApeDialogs;
+using ApeFree.ApeForms.Forms.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApeFree.ApeForms.Forms.Extensions
+{
+    public static class DialogFactoryExtension
+    {
+        public static ApeFormsDialogProvider GetApeFormsDialogProvider(this DialogFactory _)
+        {
+            return new ApeFormsDialogProvider();
+        }
+    }
+}
