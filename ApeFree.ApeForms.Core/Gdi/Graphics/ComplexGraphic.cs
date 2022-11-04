0b89b818ce06d1279ad4f5340f76afa454f7d698
Guijie Lee <landriesnidis@yeah.net>
2022-11-04 23:52:36 +0800

    增加复合图形类型

---
+++ ApeFree.ApeForms.Core/Gdi/Graphics/ComplexGraphic.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApeFree.ApeForms.Core.Gdi.Graphics
+{
+    /// <summary>
+    /// 复合图形
+    /// </summary>
+    public class ComplexGraphic : IGraphic
+    {
+        public ComplexGraphic()
+        {
+            Graphics = new LinkedList<IGraphic>();
+        }
+
+        public LinkedList<IGraphic> Graphics { get; }
+
+        public IEnumerable<PointF> Points => Graphics.SelectMany(g => g.Points);
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="centralPoint"></param>
+        /// <param name="angle"></param>
+        public void Rotate(PointF centralPoint, float angle)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="scaling"></param>
+        public void Scale(float scaling)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        /// <param name="distanceX"></param>
+        /// <param name="distanceY"></param>
+        public void Translation(float distanceX, float distanceY)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
