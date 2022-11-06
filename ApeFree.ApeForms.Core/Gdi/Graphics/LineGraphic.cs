e5801cd71e3845fb8d6acebb8315f45a16616009
Guijie Lee <landriesnidis@yeah.net>
2022-11-06 22:39:35 +0800

    增加线图形类型

---
+++ ApeFree.ApeForms.Core/Gdi/Graphics/LineGraphic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ApeFree.ApeForms.Core.Gdi.Graphics
+{
+    public class LineGraphic : IGraphic
+    {
+        public PointF StartPoint { get; set; }
+
+        public PointF EndPoint { get; set; }
+
+        public IEnumerable<PointF> Points => new PointF[] { StartPoint, EndPoint };
+
+        private PointF CentrePoint => new PointF((StartPoint.X + EndPoint.X) / 2, (StartPoint.Y + EndPoint.Y) / 2);
+
+        public void Rotate(PointF centralPoint, float angle)
+        {
+            var cp = CentrePoint;
+            StartPoint = GdiMath.PointAround(cp, StartPoint, angle);
+            EndPoint = GdiMath.PointAround(cp, EndPoint, angle);
+        }
+
+        public void Scale(float scaling)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Translation(float distanceX, float distanceY)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
