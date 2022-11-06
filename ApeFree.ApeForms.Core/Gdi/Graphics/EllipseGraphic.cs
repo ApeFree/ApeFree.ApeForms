5fef8880b00b3c38738551e692d6770ede61cee1
Guijie Lee <landriesnidis@yeah.net>
2022-11-06 23:16:50 +0800

    增加椭圆图形类型

---
+++ ApeFree.ApeForms.Core/Gdi/Graphics/EllipseGraphic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace ApeFree.ApeForms.Core.Gdi.Graphics
+{
+    public class EllipseGraphic : IGraphic
+    {
+        public IEnumerable<PointF> Points => throw new NotImplementedException();
+
+        public void Rotate(PointF centralPoint, float angle)
+        {
+            return;
+        }
+
+        public void Scale(float scaling)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Translation(float distanceX, float distanceY)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
