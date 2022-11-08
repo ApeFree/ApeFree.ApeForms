82da988fcfec06d5c14186f43b2d88ffd245d8be
Guijie Lee <landriesnidis@yeah.net>
2022-11-09 01:17:16 +0800

    实现图形面板基类

--- ApeFree.ApeForms.Core/Gdi/Palette.cs
+++ ApeFree.ApeForms.Core/Gdi/Palette.cs
@@ -3,9 +3,55 @@
 namespace ApeFree.ApeForms.Core.Gdi
 {
     /// <summary>
-    /// 图形面板
+    /// 图形面板基类
     /// </summary>
-    public class Palette
+    /// <typeparam name="TStyle">绘制风格类型</typeparam>
+    public abstract class Palette<TStyle> : IDisposable
+    {
+        /// <summary>
+        /// 图层
+        /// </summary>
+        public List<Layer<TStyle>> Layers { get; protected internal set; }
+
+        /// <summary>
+        /// 构造画板
+        /// </summary>
+        protected internal Palette()
+        {
+            Layers = new List<Layer<TStyle>>();
+        }
+
+        /// <summary>
+        /// <inheritdoc/>
+        /// </summary>
+        public abstract void Dispose();
+
+        protected abstract void DrawLine(TStyle style, LineShape graphic);
+        protected abstract void DrawEllipse(TStyle style, EllipseShape graphic);
+    }
+
+    /// <summary>
+    /// 图形面板基类
+    /// </summary>
+    /// <typeparam name="TCanvas">画布类型</typeparam>
+    /// <typeparam name="TStyle">绘制风格类型</typeparam>
+    public abstract class Palette<TCanvas, TStyle> : Palette<TStyle>
+    {
+        /// <summary>
+        /// 画布
+    /// </summary>
+        protected TCanvas Canvas { get; set; }
+
+        /// <summary>
+        /// 构造画板
+        /// </summary>
+        protected Palette() { }
+
+        /// <summary>
+        /// 构造画板
+        /// </summary>
+        /// <param name="canvas">画布对象</param>
+        protected Palette(TCanvas canvas) : this()
     {
         public List<IGraphic> Graphics { get; }
 
