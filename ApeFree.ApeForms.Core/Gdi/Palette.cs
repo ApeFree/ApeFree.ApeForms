af517182a17c0582d57f4ba8e1df2dd0fbfb2dd5
Guijie Lee <landriesnidis@yeah.net>
2022-11-08 23:55:46 +0800

    增加图形画板

---
+++ ApeFree.ApeForms.Core/Gdi/Palette.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace ApeFree.ApeForms.Core.Gdi
+{
+    /// <summary>
+    /// 图形面板
+    /// </summary>
+    public class Palette
+    {
+        public List<IGraphic> Graphics { get; }
+
+        protected Palette()
+        {
+            Graphics = new List<IGraphic>();
+        }
+    }
+
+    /// <summary>
+    /// Gdi图形画板
+    /// </summary>
+    public class GdiPalette : Palette
+    {
+
+    }
+}
