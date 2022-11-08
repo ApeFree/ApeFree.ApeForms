1a95f384cda72705d2fa9022ed0927ca88bd8dc7
Guijie Lee <landriesnidis@yeah.net>
2022-11-09 00:41:39 +0800

    增加图层概念

---
+++ ApeFree.ApeForms.Core/Gdi/Layer.cs
@@ -0,0 +1,23 @@
+﻿namespace ApeFree.ApeForms.Core.Gdi
+{
+    /// <summary>
+    /// 图层
+    /// </summary>
+    public class Layer<TStyle>
+    {
+        /// <summary>
+        /// 风格样式
+        /// </summary>
+        public TStyle Style { get; set; }
+
+        /// <summary>
+        /// 图形
+        /// </summary>
+        public IShape Shape { get; set; }
+
+        /// <summary>
+        /// 可见性
+        /// </summary>
+        public bool Visible { get; set; }
+    }
+}
