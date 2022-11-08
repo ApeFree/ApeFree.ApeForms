fd9a0eadca0785bd4d494d5f6ef98cd3709a4998
Guijie Lee <landriesnidis@yeah.net>
2022-11-09 01:16:27 +0800

    增加指向父类容器的属性，实现双向关联

--- ApeFree.ApeForms.Core/Gdi/Layer.cs
+++ ApeFree.ApeForms.Core/Gdi/Layer.cs
@@ -19,5 +19,32 @@
         /// 可见性
         /// </summary>
         public bool Visible { get; set; }
+
+        /// <summary>
+        /// 画板
+        /// </summary>
+        public Palette<TStyle> Parent { get; }
+
+        /// <summary>
+        /// 构造图层
+        /// </summary>
+        /// <param name="parent">所属画板</param>
+        internal Layer(Palette<TStyle> parent)
+        {
+            Visible = true;
+            Parent = parent;
+        }
+
+        /// <summary>
+        /// 构造图层
+        /// </summary>
+        /// <param name="parent">所属画板</param>
+        /// <param name="style">样式风格</param>
+        /// <param name="shape">图形</param>
+        internal Layer(Palette<TStyle> parent, TStyle style, IShape shape) : this(parent)
+        {
+            Style = style;
+            Shape = shape;
+        }
     }
 }
