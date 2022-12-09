14fc723e0bb9b516bc603b7f863587d05e8c1a86
Guijie Lee <landriesnidis@yeah.net>
2022-12-10 01:34:50 +0800

    增加线长度属性

--- ApeFree.ApeForms.Core/Gdi/Shapes/LineShape.cs
+++ ApeFree.ApeForms.Core/Gdi/Shapes/LineShape.cs
@@ -14,6 +14,11 @@ namespace ApeFree.ApeForms.Core.Gdi.Shapes
 
         private PointF CentrePoint => new PointF((StartPoint.X + EndPoint.X) / 2, (StartPoint.Y + EndPoint.Y) / 2);
 
+        /// <summary>
+        /// 线长
+        /// </summary>
+        public double Length => GdiMath.CalculateLengthFromTwoPoints(StartPoint, EndPoint);
+
         public void Rotate(PointF centralPoint, float angle)
         {
             var cp = CentrePoint;
@@ -35,6 +40,38 @@ namespace ApeFree.ApeForms.Core.Gdi.Shapes
         public bool Contains(PointF point)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 构造线图形
+        /// </summary>
+        /// <param name="x1">起始点X坐标</param>
+        /// <param name="y1">起始点Y坐标</param>
+        /// <param name="x2">结束点X坐标</param>
+        /// <param name="y2">结束点Y坐标</param>
+        public LineShape(float x1, float y1, float x2, float y2) : this(new PointF(x1, y1), new PointF(x2, y2)) { }
+
+        /// <summary>
+        /// 构造线图形
+        /// </summary>
+        /// <param name="startPoint">起始点</param>
+        /// <param name="endPoint">结束点</param>
+        public LineShape(PointF startPoint, PointF endPoint)
+        {
+            StartPoint = startPoint;
+            EndPoint = endPoint;
+        }
+
+        /// <summary>
+        /// 构造线图形
+        /// </summary>
+        /// <param name="startPoint">起始点</param>
+        /// <param name="length">长度</param>
+        /// <param name="angle">角度</param>
+        public LineShape(PointF startPoint, double length, float angle)
+        {
+            StartPoint = startPoint;
+            EndPoint = GdiMath.CalculatePointOnCircle(startPoint, (float)length, angle);
         }
     }
 }
