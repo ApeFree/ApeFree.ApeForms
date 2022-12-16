5c63604bc596551305635ad14584c925ee19ab90
Guijie Lee <landriesnidis@yeah.net>
2022-12-17 01:40:28 +0800

    绘制时钟的DEMO面板

---
+++ ApeFree.ApeForms.Demo/DemoPanel/GdiPaletteDemoPanel.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace ApeFree.ApeForms.Demo.DemoPanel
+{
+    partial class GdiPaletteDemoPanel
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerClock = new System.Windows.Forms.Timer(this.components);
+            this.gbCanvas = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // timerClock
+            // 
+            this.timerClock.Enabled = true;
+            this.timerClock.Interval = 500;
+            this.timerClock.Tick += new System.EventHandler(this.timerClock_Tick);
+            // 
+            // gbCanvas
+            // 
+            this.gbCanvas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbCanvas.Location = new System.Drawing.Point(0, 0);
+            this.gbCanvas.Name = "gbCanvas";
+            this.gbCanvas.Size = new System.Drawing.Size(560, 363);
+            this.gbCanvas.TabIndex = 0;
+            this.gbCanvas.TabStop = false;
+            this.gbCanvas.Text = "Canvas";
+            // 
+            // GdiPaletteDemoPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gbCanvas);
+            this.Name = "GdiPaletteDemoPanel";
+            this.Size = new System.Drawing.Size(560, 363);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timerClock;
+        private System.Windows.Forms.GroupBox gbCanvas;
+    }
+}
---
+++ ApeFree.ApeForms.Demo/DemoPanel/GdiPaletteDemoPanel.cs
@@ -0,0 +1,98 @@
+﻿using ApeFree.ApeForms.Core.Gdi;
+using ApeFree.ApeForms.Core.Gdi.Shapes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApeFree.ApeForms.Demo.DemoPanel
+{
+    public partial class GdiPaletteDemoPanel : UserControl
+    {
+        // 表盘中心点
+        private PointF centralPoint = new PointF(200, 200);
+        // 表盘半径
+        private int clockRadius = 150;
+
+        // 画板
+        private readonly GdiPalette palette;
+        // 时分秒针图层
+        private readonly Layer<GdiPalette.ShapeStyle> layerHour;
+        private readonly Layer<GdiPalette.ShapeStyle> layerMinute;
+        private readonly Layer<GdiPalette.ShapeStyle> layerSecond;
+
+        /// <summary>
+        /// 画布控件
+        /// </summary>
+        private Control Canvas => gbCanvas;
+
+        public GdiPaletteDemoPanel()
+        {
+            InitializeComponent();
+
+            // 为当做画布的控件设置重绘事件
+            Canvas.Paint += GbCanvas_Paint;
+
+            // 创建GDI+画板
+            palette = new GdiPalette();
+
+            // 绘制圆形表盘和刻度
+            palette.Draw(new GdiPalette.ShapeStyle() { Pen = new Pen(Color.Black, 3) }, new EllipseShape(centralPoint.X - clockRadius, centralPoint.Y - clockRadius, clockRadius * 2, clockRadius * 2));
+            for (int i = 0; i < 60; i++)
+            {
+                palette.Draw(new GdiPalette.ShapeStyle() { Pen = new Pen(Color.DarkGray, 2) }, new LineShape(GdiMath.CalculatePointOnCircle(centralPoint, clockRadius - 10, i * 6), GdiMath.CalculatePointOnCircle(centralPoint, clockRadius - (i % 5 == 0 ? 25 : 15), i * 6)));
+            }
+
+            // 绘制时针分针秒针，并将图层存到全局
+            layerSecond = palette.Draw(new GdiPalette.ShapeStyle() { Pen = new Pen(Color.DarkRed, 1) }, new LineShape(new PointF(200, 200), 140, -90));
+            layerMinute = palette.Draw(new GdiPalette.ShapeStyle() { Pen = new Pen(Color.DarkGreen, 3) }, new LineShape(new PointF(200, 200), 120, -93));
+            layerHour = palette.Draw(new GdiPalette.ShapeStyle() { Pen = new Pen(Color.DarkBlue, 5) }, new LineShape(new PointF(200, 200), 100, -96));
+        }
+
+        private void GbCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            // 创建空白图
+            Bitmap bmp = new Bitmap(Canvas.Width, Canvas.Height);
+            Graphics graph = Graphics.FromImage(bmp);
+            graph.Clear(Canvas.BackColor);
+
+            // 将画板内容绘制到空白图
+            palette.Canvas = graph;
+            palette.Refresh();
+
+            // 将图绘制到控件
+            e.Graphics.DrawImage(bmp, Canvas.ClientRectangle);
+            e.Graphics.Dispose();
+        }
+
+        private void timerClock_Tick(object sender, EventArgs e)
+        {
+            // 获取当前时间，并计算当前是今日的第几秒
+            var now = DateTime.Now;
+            var totalSeconds = now.Hour * 3600 + now.Minute * 60 + now.Second;
+
+            // 根据当前时间计算时针分针秒针的指向
+            (layerSecond.Shape as LineShape).With(shape =>
+            {
+                shape.EndPoint = GdiMath.CalculatePointOnCircle(shape.StartPoint, (float)shape.Length, totalSeconds % 60 / 60f * 360 - 90);
+            });
+            (layerMinute.Shape as LineShape).With(shape =>
+            {
+                shape.EndPoint = GdiMath.CalculatePointOnCircle(shape.StartPoint, (float)shape.Length, totalSeconds % 3600 / 3600f * 360 - 90);
+            });
+            (layerHour.Shape as LineShape).With(shape =>
+            {
+                shape.EndPoint = GdiMath.CalculatePointOnCircle(shape.StartPoint, (float)shape.Length, totalSeconds / 86400f * 360 - 90);
+            });
+
+            // 刷新画布控件
+            Canvas.Refresh();
+        }
+    }
+}
