bcf1af6b06e5dea5f0759c56c4208d8b1a8033af
Guijie Lee <landriesnidis@yeah.net>
2023-02-05 03:35:23 +0800

    新增Notification通知栏接口

---
+++ ApeFree.ApeForms.Forms/Notifications/NotificationBox/INotification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApeFree.ApeForms.Forms.Notifications
+{
+    public interface INotification
+    {
+        Control ContentView { get; }
+        Control SpareView { get; }
+
+        void Active();
+        Control AddOption(string text, NotificationBox.OptionClickEventHandler onClick);
+        void Disappear();
+    }
+}
