2ea09a69eaee8033b110786890a64117ab44481d
Guijie Lee <landriesnidis@yeah.net>
2023-02-08 03:36:00 +0800

    实现通知栏功能

---
+++ ApeFree.ApeForms.Forms/Notifications/NotificationBox/NotificationBox.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ApeFree.ApeForms.Forms.Notifications
+{
+    partial class NotificationBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerDisappear = new System.Windows.Forms.Timer(this.components);
+            this.labTitle = new System.Windows.Forms.Label();
+            this.btnClose = new ApeFree.ApeForms.Core.Controls.ShapeButton();
+            this.flpOptions = new System.Windows.Forms.FlowLayoutPanel();
+            this.panelSpareControl = new System.Windows.Forms.Panel();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.panelMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timerDisappear
+            // 
+            this.timerDisappear.Interval = 5000;
+            this.timerDisappear.Tick += new System.EventHandler(this.timerDisappear_Tick);
+            // 
+            // labTitle
+            // 
+            this.labTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labTitle.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labTitle.Location = new System.Drawing.Point(3, 3);
+            this.labTitle.Name = "labTitle";
+            this.labTitle.Size = new System.Drawing.Size(294, 20);
+            this.labTitle.TabIndex = 1;
+            this.labTitle.Text = "Notification";
+            this.labTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(196)))), ((int)(((byte)(196)))), ((int)(((byte)(196)))));
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.Black;
+            this.btnClose.GraphicScale = 0.5F;
+            this.btnClose.Location = new System.Drawing.Point(277, 3);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Shape = ApeFree.ApeForms.Core.Controls.ShapeButton.SimpleShape.Close;
+            this.btnClose.Size = new System.Drawing.Size(20, 20);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.TabStop = false;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // flpOptions
+            // 
+            this.flpOptions.AutoSize = true;
+            this.flpOptions.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flpOptions.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flpOptions.Location = new System.Drawing.Point(3, 97);
+            this.flpOptions.Name = "flpOptions";
+            this.flpOptions.Size = new System.Drawing.Size(294, 0);
+            this.flpOptions.TabIndex = 2;
+            // 
+            // panelSpareControl
+            // 
+            this.panelSpareControl.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelSpareControl.Location = new System.Drawing.Point(4, 4);
+            this.panelSpareControl.Name = "panelSpareControl";
+            this.panelSpareControl.Size = new System.Drawing.Size(61, 66);
+            this.panelSpareControl.TabIndex = 3;
+            this.panelSpareControl.Visible = false;
+            this.panelSpareControl.SizeChanged += new System.EventHandler(this.PanelSpareControl_SizeChanged);
+            // 
+            // panelMain
+            // 
+            this.panelMain.Controls.Add(this.panelSpareControl);
+            this.panelMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMain.Location = new System.Drawing.Point(3, 23);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Padding = new System.Windows.Forms.Padding(4);
+            this.panelMain.Size = new System.Drawing.Size(294, 74);
+            this.panelMain.TabIndex = 4;
+            // 
+            // NotificationBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(300, 100);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.flpOptions);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.labTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NotificationBox";
+            this.Padding = new System.Windows.Forms.Padding(3);
+            this.TopMost = true;
+            this.panelMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerDisappear;
+        private Core.Controls.ShapeButton btnClose;
+        private System.Windows.Forms.Label labTitle;
+        private System.Windows.Forms.FlowLayoutPanel flpOptions;
+        private System.Windows.Forms.Panel panelSpareControl;
+        private System.Windows.Forms.Panel panelMain;
+    }
+}
---
+++ ApeFree.ApeForms.Forms/Notifications/NotificationBox/NotificationBox.cs
@@ -0,0 +1,322 @@
+﻿using ApeFree.CodePlus.Algorithm.DataStructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using ApeFree.ApeForms.Core.Controls;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace ApeFree.ApeForms.Forms.Notifications
+{
+    public partial class NotificationBox : Form, INotification
+    {
+        /// <summary>
+        /// 选项单击事件委托
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        public delegate void OptionClickEventHandler(object sender, OptionClickEventArgs e);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern bool ShowWindow(HandleRef hWnd, int nCmdShow);
+
+        public int DisappearInterval { get; set; } = 5000;
+
+        internal bool IsDisappear { get; set; } = false;
+
+        /// <summary>
+        /// 内容控件
+        /// </summary>
+        public Control ContentView { get; }
+
+        /// <summary>
+        /// 备用控件
+        /// </summary>
+        public Control SpareView { get; }
+
+        private NotificationBox()
+        {
+            InitializeComponent();
+            Size = DefaultFormsSize;
+        }
+
+        internal NotificationBox(Control contentView, Control spareView = null)
+        {
+            InitializeComponent();
+            Size = DefaultFormsSize;
+
+            ContentView = contentView;
+            SpareView = spareView;
+
+            if (ContentView != null)
+            {
+                ContentView.Dock = DockStyle.Fill;
+                panelMain.Controls.Add(ContentView);
+                ContentView.BringToFront();
+            }
+
+            if (SpareView != null)
+            {
+                SpareView.Dock = DockStyle.Fill;
+                panelSpareControl.Controls.Add(SpareView);
+            }
+
+            panelSpareControl.Visible = SpareView != null;
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            labTitle.Text = Text;
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+
+            e.Cancel = IsDisappear;
+
+            if (!IsDisappear)
+            {
+                Disappear();
+            }
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            base.OnPaintBackground(e);
+
+            Pen pen = new Pen(BackColor.Luminance(0.5f), 1);
+            e.Graphics.DrawRectangle(pen, new Rectangle(0, 0, Width - 1, Height - 1));
+        }
+
+        private void PanelSpareControl_SizeChanged(object sender, EventArgs e)
+        {
+            panelSpareControl.Width = panelSpareControl.Height;
+        }
+
+        /// <summary>
+        /// 显示窗体
+        /// </summary>
+        public new void Show()
+        {
+            ShowWindow(new HandleRef(this, this.Handle), 4);
+            timerDisappear.Interval = DisappearInterval;
+            timerDisappear.Enabled = true;
+        }
+
+        /// <summary>
+        /// 激活窗体，重置消失倒计时
+        /// </summary>
+        public void Active()
+        {
+            timerDisappear.Enabled = false;
+            timerDisappear.Enabled = true;
+        }
+
+        /// <summary>
+        /// 使窗体消失
+        /// </summary>
+        public void Disappear()
+        {
+            IsDisappear = true;
+            timerDisappear.Enabled = false;
+            NotifyForms.Remove(this);
+            Close();
+            Dispose();
+        }
+
+        /// <summary>
+        /// 添加选项
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="onClick"></param>
+        /// <returns></returns>
+        public Control AddOption(string text, OptionClickEventHandler onClick)
+        {
+            var btn = new SimpleButton();
+            btn.Text = text;
+            btn.Size = new Size(65, 20);
+            btn.BackColor = Color.LightGray;
+            btn.ForeColor = Color.Black;
+            btn.AutoSize = true;
+            btn.TabStop = false;
+            btn.Click += (s, e) =>
+            {
+                var args = new OptionClickEventArgs(this, btn);
+                onClick?.Invoke(btn, args);
+
+                if (args.IsDisappear)
+                {
+                    Disappear();
+                }
+            };
+
+            flpOptions.Controls.Add(btn);
+
+            return btn;
+        }
+
+        private void timerDisappear_Tick(object sender, EventArgs e)
+        {
+            Disappear();
+        }
+
+        // ==============================================================================================================================
+
+        internal static readonly EventableList<NotificationBox> NotifyForms;
+
+        /// <summary>
+        /// 通知栏之间的间隔距离
+        /// </summary>
+        public static int SpacingDistance { get; set; } = 10;
+
+        /// <summary>
+        /// 通知栏默认大小
+        /// </summary>
+        public static Size DefaultFormsSize { get; set; } = new Size(300, 100);
+
+        /// <summary>
+        /// 通知排列方向
+        /// </summary>
+        public static NotifyOrientation Orientation { get; set; } = NotifyOrientation.Stack;
+
+        /// <summary>
+        /// 通知起始方向
+        /// </summary>
+        public static NotifyPrimeDirection PrimeDirection { get; set; } = NotifyPrimeDirection.Bottom;
+
+        static NotificationBox()
+        {
+            NotifyForms = new EventableList<NotificationBox>();
+
+            NotifyForms.ItemAdded += NotifyForms_ItemChanged;
+            NotifyForms.ItemInserted += NotifyForms_ItemChanged;
+            NotifyForms.ItemRemoved += NotifyForms_ItemChanged;
+        }
+
+        public static INotification Publish(string title, Control contentView, Control spareView = null, uint delay = 10000)
+        {
+            var form = new NotificationBox(contentView, spareView);
+            form.Text = title;
+            form.DisappearInterval = (int)delay;
+
+            NotifyForms.Add(form);
+            return form;
+        }
+
+        public static INotification Publish(string title, string content, uint delay = 10000)
+        {
+            var contentView = new Label { Text = content, AutoSize = false};
+            return Publish(title, contentView, null, delay);
+        }
+
+        public static INotification Publish(string title, string content, Image image, uint delay = 10000)
+        {
+            var contentView = new Label { Text = content, AutoSize = false, Padding = new Padding(5, 0, 0, 0) };
+            var spareView = new PictureBox() { Image = image, SizeMode = PictureBoxSizeMode.Zoom };
+            return Publish(title, contentView, spareView, delay);
+        }
+
+
+        private static void NotifyForms_ItemChanged(object sender, ListItemsChangedEventArgs<NotificationBox> e)
+        {
+            // 获取屏幕
+            var screen = Screen.AllScreens.FirstOrDefault();
+            if (screen == null)
+            {
+                throw new Exception("No screens are currently available");
+            }
+
+            for (int i = 0; i < NotifyForms.Count; ++i)
+            {
+                NotificationBox form = NotifyForms[i];
+                var point = CalculateLastPosition(screen, i);
+                if (form.Visible)
+                {
+                    form.LocationGradualChange(point);
+                }
+                else
+                {
+                    form.Show();
+                    form.Location = point;
+                }
+            }
+        }
+
+        private static Point CalculateLastPosition(Screen screen, int index)
+        {
+            // 获取工作区宽高
+            var h = screen.WorkingArea.Height;
+            var w = screen.WorkingArea.Width;
+
+            // 排列方向
+            if (Orientation == NotifyOrientation.Stack)
+            {
+                index = NotifyForms.Count - index - 1;
+            }
+
+            var top = 0;
+            if (PrimeDirection == NotifyPrimeDirection.Top)
+            {
+                top = (DefaultFormsSize.Height + SpacingDistance) * index + SpacingDistance;
+            }
+            else if (PrimeDirection == NotifyPrimeDirection.Bottom)
+            {
+                top = h - (DefaultFormsSize.Height + SpacingDistance) * index - DefaultFormsSize.Height;
+            }
+
+            var left = w - SpacingDistance - DefaultFormsSize.Width;
+
+            return new Point(left, top);
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Disappear();
+        }
+    }
+
+    /// <summary>
+    /// 通知排列方向
+    /// </summary>
+    public enum NotifyOrientation
+    {
+        Queue,
+        Stack,
+    }
+
+    /// <summary>
+    /// 通知起始方向
+    /// </summary>
+    public enum NotifyPrimeDirection
+    {
+        Top,
+        Bottom,
+    }
+
+    /// <summary>
+    /// 选项单击事件
+    /// </summary>
+    public class OptionClickEventArgs : EventArgs
+    {
+        public OptionClickEventArgs(NotificationBox notificationBox, Control optionControl)
+        {
+            NotificationBox = notificationBox;
+            OptionControl = optionControl;
+            IsDisappear = true;
+        }
+
+        public NotificationBox NotificationBox { get; }
+        public Control OptionControl { get; }
+        public bool IsDisappear { get; set; }
+    }
+}
