1aaf89a0f9fdb2697ca35bf3439a0ac25f85e468
Guijie Lee <landriesnidis@yeah.net>
2023-02-10 03:34:05 +0800

    移除无效的引用

--- ApeFree.ApeForms.Forms/Notifications/NotificationBox/NotificationBox.cs
+++ ApeFree.ApeForms.Forms/Notifications/NotificationBox/NotificationBox.cs
@@ -1,17 +1,11 @@
 ﻿using ApeFree.CodePlus.Algorithm.DataStructure;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime.InteropServices;
 using ApeFree.ApeForms.Core.Controls;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace ApeFree.ApeForms.Forms.Notifications
 {
