24873aa76510aa3473b82a64fa7e5bd482707d7a
Guijie Lee <landriesnidis@yeah.net>
2023-02-22 23:24:00 +0800

    新增选项按钮类型OptionButton

---
+++ ApeFree.ApeForms.Forms/Dialogs/OptionButton.cs
@@ -0,0 +1,16 @@
+﻿using ApeFree.ApeForms.Core.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApeFree.ApeForms.Forms.Dialogs
+{
+    [ToolboxItem(false)]
+    public class OptionButton : SimpleButton
+    {
+
+    }
+}
