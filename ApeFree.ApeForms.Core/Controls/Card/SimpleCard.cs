d171140d5364b2802e2ccb4d119badbaf36dbac6
Guijie Lee <landriesnidis@yeah.net>
2023-03-15 22:46:01 +0800

    消除无效的引用

--- ApeFree.ApeForms.Core/Controls/Card/SimpleCard.cs
+++ ApeFree.ApeForms.Core/Controls/Card/SimpleCard.cs
@@ -1,12 +1,6 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ApeFree.ApeForms.Core.Controls
