c898c461eed368c837b0b5f725669d6ebe65931c
Guijie Lee <landriesnidis@yeah.net>
2023-06-19 03:27:50 +0800

    重构Palette、优化GDI+Palette的实现

--- ApeFree.ApeForms.Core/Gdi/Palette.cs
+++ ApeFree.ApeForms.Core/Gdi/Palette.cs
@@ -1,8 +1,8 @@
-﻿using ApeFree.ApeForms.Core.Gdi.Shapes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Linq;
 
 namespace ApeFree.ApeForms.Core.Gdi
 {
@@ -12,26 +12,24 @@ namespace ApeFree.ApeForms.Core.Gdi
     /// <typeparam name="TStyle">绘制风格类型</typeparam>
     public abstract class Palette<TStyle> : IDisposable
     {
-        /// <summary>
-        /// 图层
-        /// </summary>
-        public IList<Layer<TStyle>> Layers { get; protected internal set; }
+        /// <summary>图层</summary>
+        public IList<Layer<TStyle>> Layers { get; private set; }
 
-        /// <summary>
-        /// 构造画板
-        /// </summary>
+        /// <summary>构造画板</summary>
         protected internal Palette()
         {
             Layers = new List<Layer<TStyle>>();
         }
 
-        /// <summary>
         /// <inheritdoc/>
-        /// </summary>
-        public abstract void Dispose();
+        public virtual void Dispose()
+        {
+            Layers.Clear();
+            Layers = null;
+        }
 
-
-        public virtual void Refresh()
+        /// <summary>更新画布</summary>
+        public virtual void UpdateCanvas()
         {
             foreach (var layer in Layers)
             {
@@ -45,28 +43,48 @@ namespace ApeFree.ApeForms.Core.Gdi
                 switch (layer.Shape)
                 {
                     case LineShape shape:
-                        DrawLine(layer.Style, shape);
+                        DrawLineHandler(layer.Style, shape);
                         break;
                     case EllipseShape shape:
-                        DrawEllipse(layer.Style, shape);
+                        DrawEllipseHandler(layer.Style, shape);
                         break;
                     case RectangleShape shape:
-                        DrawRectangle(layer.Style, shape);
+                        DrawRectangleHandler(layer.Style, shape);
+                        break;
+                    case CircleShape shape:
+                        DrawCircleHandler(layer.Style, shape);
                         break;
                 }
             }
         }
 
-        protected abstract void DrawLine(TStyle style, LineShape graphic);
-        protected abstract void DrawEllipse(TStyle style, EllipseShape graphic);
-        protected abstract void DrawRectangle(TStyle style, RectangleShape graphic);
+        /// <summary>绘制线</summary>
+        public Layer<TStyle, LineShape> DrawLine(TStyle style, LineShape graphic) => Draw(style, graphic);
 
-        public Layer<TStyle> Draw(TStyle style, IShape graphic)
+        /// <summary>绘制椭圆</summary>
+        public Layer<TStyle, EllipseShape> DrawEllipse(TStyle style, EllipseShape graphic) => Draw(style, graphic);
+
+        /// <summary>绘制矩形</summary>
+        public Layer<TStyle, RectangleShape> DrawRectangle(TStyle style, RectangleShape graphic) => Draw(style, graphic);
+
+        /// <summary>绘制圆形</summary>
+        public Layer<TStyle, CircleShape> DrawCircle(TStyle style, CircleShape graphic) => Draw(style, graphic);
+
+        public Layer<TStyle, TShape> Draw<TShape>(TStyle style, TShape graphic) where TShape : IShape
         {
-            var layer = new Layer<TStyle>(this, style, graphic);
+            var layer = new Layer<TStyle, TShape>(this, style, graphic);
             Layers.Add(layer);
             return layer;
         }
+
+        /// <summary>绘制线的实现过程</summary>
+        protected abstract void DrawEllipseHandler(TStyle style, EllipseShape graphic);
+        /// <summary>绘制椭圆的实现过程</summary>
+        protected abstract void DrawLineHandler(TStyle style, LineShape graphic);
+        /// <summary>绘制矩形的实现过程</summary> 
+        protected abstract void DrawRectangleHandler(TStyle style, RectangleShape graphic);
+        /// <summary>绘制圆形的实现过程</summary> 
+        protected abstract void DrawCircleHandler(TStyle style, CircleShape graphic);
     }
 
     /// <summary>
@@ -94,92 +112,116 @@ namespace ApeFree.ApeForms.Core.Gdi
         {
             Canvas = canvas;
         }
-
-        /// <summary>
-        /// <inheritdoc/>
-        /// </summary>
-        public override void Dispose()
-        {
-            Layers.Clear();
-            Layers = null;
-        }
     }
 
     /// <summary>
     /// WinForm Gdi+图形画板
     /// </summary>
-    public class GdiPalette : Palette<Graphics, GdiPalette.ShapeStyle>
+    public class GdiPalette : Palette<Graphics, GdiStyle>
     {
-        /// <summary>
-        /// 绘制圆形
-        /// </summary>
-        /// <param name="style"></param>
-        /// <param name="graphic"></param>
-        protected override void DrawEllipse(ShapeStyle style, EllipseShape graphic)
+        private Rectangle clientRectangle;
+
+        public Bitmap Image { get; private set; }
+
+        public Rectangle ClientRectangle
         {
-            if (style.Pen != null)
+            get => clientRectangle;
+            set
             {
-                Canvas.DrawEllipse(style.Pen, graphic.Location.X, graphic.Location.Y, graphic.Size.Width, graphic.Size.Height);
-            }
-
-            if (style.Brush != null)
-            {
-                Canvas.FillEllipse(style.Brush, graphic.Location.X, graphic.Location.Y, graphic.Size.Width, graphic.Size.Height);
-            }
-        }
-
-        /// <summary>
-        /// 绘制线
-        /// </summary>
-        /// <param name="style"></param>
-        /// <param name="graphic"></param>
-        protected override void DrawLine(ShapeStyle style, LineShape graphic)
-        {
-            Canvas.DrawLine(style.Pen, graphic.StartPoint, graphic.EndPoint);
-        }
-
-        /// <summary>
-        /// 绘制矩形
-        /// </summary>
-        /// <param name="style"></param>
-        /// <param name="graphic"></param>
-        protected override void DrawRectangle(ShapeStyle style, RectangleShape graphic)
-        {
-            if (style.Pen != null)
-            {
-                Canvas.DrawRectangle(style.Pen, graphic.Location.X, graphic.Location.Y, graphic.Size.Width, graphic.Size.Height);
-            }
-
-            if (style.Brush != null)
-            {
-                Canvas.FillRectangle(style.Brush, graphic.Location.X, graphic.Location.Y, graphic.Size.Width, graphic.Size.Height);
+                if (clientRectangle != value)
+                {
+                    // 这里还有优化的空间
+                    clientRectangle = value;
+                    UpdateClientRectangle();
+                }
             }
         }
 
-        /// <summary>
-        /// 图形样式
-        /// </summary>
-        public class ShapeStyle
+        private void UpdateClientRectangle()
         {
-            /// <summary>
-            /// 画笔
-            /// </summary>
-            public Pen Pen { get; set; }
-
-            /// <summary>
-            /// 画刷
-            /// </summary>
-            public Brush Brush { get; set; }
+            Image?.Dispose();
+            Canvas?.Dispose();
+            Image = new Bitmap(clientRectangle.Width, clientRectangle.Height);
+            Canvas = Graphics.FromImage(Image);
         }
 
-        public override void Refresh()
+        ///<inheritdoc/>
+        public override void UpdateCanvas()
         {
             Canvas.SmoothingMode = SmoothingMode.AntiAlias;
             Canvas.SmoothingMode = SmoothingMode.HighQuality;
             Canvas.CompositingQuality = CompositingQuality.HighQuality;
             Canvas.InterpolationMode = InterpolationMode.HighQualityBicubic;
 
-            base.Refresh();
+            base.UpdateCanvas();
         }
+
+        ///<inheritdoc/>
+        protected override void DrawEllipseHandler(GdiStyle style, EllipseShape graphic)
+        {
+            var p = graphic.Points.First();
+            if (style.Pen != null)
+            {
+                Canvas.DrawEllipse(style.Pen, p.X, p.Y, graphic.Width, graphic.Height);
+            }
+
+            if (style.Brush != null)
+            {
+                Canvas.FillEllipse(style.Brush, p.X, p.Y, graphic.Width, graphic.Height);
+            }
+        }
+
+        ///<inheritdoc/>
+        protected override void DrawLineHandler(GdiStyle style, LineShape graphic)
+        {
+            Canvas.DrawLine(style.Pen, graphic.StartPoint, graphic.EndPoint);
+        }
+
+        ///<inheritdoc/>
+        protected override void DrawRectangleHandler(GdiStyle style, RectangleShape graphic)
+        {
+            var p = graphic.Points.First();
+            if (style.Pen != null)
+            {
+                Canvas.DrawRectangle(style.Pen, p.X, p.Y, graphic.Width, graphic.Height);
+            }
+
+            if (style.Brush != null)
+            {
+                Canvas.FillRectangle(style.Brush, p.X, p.Y, graphic.Width, graphic.Height);
+            }
+        }
+
+        ///<inheritdoc/>
+        protected override void DrawCircleHandler(GdiStyle style, CircleShape graphic)
+        {
+            var p = graphic.CenterPoint;
+            var r = graphic.Radius;
+            if (style.Pen != null)
+            {
+                Canvas.DrawEllipse(style.Pen, p.X - r, p.Y - r, r, r);
+            }
+
+            if (style.Brush != null)
+            {
+                Canvas.FillEllipse(style.Brush, p.X - r, p.Y - r, r, r);
+            }
+        }
+    }
+
+    /// <summary>
+    /// GDI+图形样式
+    /// </summary>
+    public class GdiStyle
+    {
+        /// <summary>
+        /// 画笔
+        /// </summary>
+        public Pen Pen { get; set; }
+
+        /// <summary>
+        /// 画刷
+        /// </summary>
+        public Brush Brush { get; set; }
     }
 }
