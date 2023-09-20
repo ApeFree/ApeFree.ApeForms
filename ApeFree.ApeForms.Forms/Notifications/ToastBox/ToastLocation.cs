05c0747655e92bbeb42734131dd204d789df45c4
Guijie Lee <landriesnidis@yeah.net>
2023-09-21 02:31:41 +0800

    新增显示位置的枚举

---
+++ ApeFree.ApeForms.Forms/Notifications/ToastBox/ToastLocation.cs
@@ -0,0 +1,23 @@
+﻿namespace ApeFree.ApeForms.Forms.Notifications
+{
+    /// <summary>
+    /// Toast显示位置
+    /// </summary>
+    public enum ToastLocation
+    {
+        /// <summary>
+        /// 自动
+        /// </summary>
+        Auto,
+        
+        /// <summary>
+        /// 屏幕相对位置
+        /// </summary>
+        Screen,
+
+        /// <summary>
+        /// 活动窗体相对位置
+        /// </summary>
+        ActiveForm,
+    }
+}
