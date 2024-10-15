9eb52699596f85704ac90c9546b164d3a0c1705c
Guijie Lee <landriesnidis@yeah.net>
2024-10-15 08:56:45 +0800

    新增驱动浏览器控件

---
+++ ApeFree.ApeForms.Core/Controls/Views/DriveBrowserView.Designer.cs
@@ -0,0 +1,259 @@
+﻿using ApeFree.ApeForms.Core.Controls;
+
+namespace ApeFree.ApeForms.Core.Controls.Views
+{
+    partial class DriveBrowserView
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DriveBrowserView));
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbPath = new ApeFree.ApeForms.Core.Controls.RoundTextPanel();
+            this.btnDrive = new ApeFree.ApeForms.Core.Controls.SimpleButton();
+            this.btnPrevious = new ApeFree.ApeForms.Core.Controls.SimpleButton();
+            this.btnGoTo = new ApeFree.ApeForms.Core.Controls.SimpleButton();
+            this.btnBack = new ApeFree.ApeForms.Core.Controls.SimpleButton();
+            this.cmsDrives = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.cmsFileContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.colName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colCteationTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView = new System.Windows.Forms.ListView();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "Folder");
+            this.imageList.Images.SetKeyName(1, "File");
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbPath);
+            this.panel1.Controls.Add(this.btnDrive);
+            this.panel1.Controls.Add(this.btnPrevious);
+            this.panel1.Controls.Add(this.btnGoTo);
+            this.panel1.Controls.Add(this.btnBack);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.panel1.Size = new System.Drawing.Size(600, 35);
+            this.panel1.TabIndex = 3;
+            // 
+            // tbPath
+            // 
+            this.tbPath.BackColor = System.Drawing.SystemColors.Window;
+            this.tbPath.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.tbPath.BorderWidth = ((ushort)(2));
+            this.tbPath.CornerRadius = 20;
+            this.tbPath.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbPath.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tbPath.Hint = null;
+            this.tbPath.HintColor = System.Drawing.Color.Gray;
+            this.tbPath.Location = new System.Drawing.Point(105, 0);
+            this.tbPath.MinimumSize = new System.Drawing.Size(30, 22);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.ReadOnly = false;
+            this.tbPath.Size = new System.Drawing.Size(460, 32);
+            this.tbPath.TabIndex = 1;
+            this.tbPath.Text = "C:\\";
+            this.tbPath.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.tbPath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbPath_KeyDown);
+            // 
+            // btnDrive
+            // 
+            this.btnDrive.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnDrive.BorderColor = System.Drawing.Color.Empty;
+            this.btnDrive.BorderSize = 1;
+            this.btnDrive.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnDrive.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(135)))), ((int)(((byte)(225)))));
+            this.btnDrive.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(99)))), ((int)(((byte)(164)))));
+            this.btnDrive.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(147)))), ((int)(((byte)(246)))));
+            this.btnDrive.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDrive.Font = new System.Drawing.Font("等线", 15F);
+            this.btnDrive.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnDrive.Icon = null;
+            this.btnDrive.IconScaling = 0.6F;
+            this.btnDrive.Location = new System.Drawing.Point(70, 0);
+            this.btnDrive.Name = "btnDrive";
+            this.btnDrive.Size = new System.Drawing.Size(35, 32);
+            this.btnDrive.TabIndex = 3;
+            this.btnDrive.Text = "💽";
+            this.btnDrive.UsePureColorIcon = true;
+            this.btnDrive.UseVisualStyleBackColor = true;
+            this.btnDrive.Click += new System.EventHandler(this.OnDriveButtonClicked);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnPrevious.BorderColor = System.Drawing.Color.Empty;
+            this.btnPrevious.BorderSize = 1;
+            this.btnPrevious.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnPrevious.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(135)))), ((int)(((byte)(225)))));
+            this.btnPrevious.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(99)))), ((int)(((byte)(164)))));
+            this.btnPrevious.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(147)))), ((int)(((byte)(246)))));
+            this.btnPrevious.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrevious.Font = new System.Drawing.Font("等线", 15F);
+            this.btnPrevious.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnPrevious.Icon = null;
+            this.btnPrevious.IconScaling = 0.6F;
+            this.btnPrevious.Location = new System.Drawing.Point(35, 0);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(35, 32);
+            this.btnPrevious.TabIndex = 4;
+            this.btnPrevious.Text = "⏫";
+            this.btnPrevious.UsePureColorIcon = true;
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.OnPreviousButtonClicked);
+            // 
+            // btnGoTo
+            // 
+            this.btnGoTo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnGoTo.BorderColor = System.Drawing.Color.Empty;
+            this.btnGoTo.BorderSize = 1;
+            this.btnGoTo.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnGoTo.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(135)))), ((int)(((byte)(225)))));
+            this.btnGoTo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(99)))), ((int)(((byte)(164)))));
+            this.btnGoTo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(147)))), ((int)(((byte)(246)))));
+            this.btnGoTo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGoTo.Font = new System.Drawing.Font("等线", 15F);
+            this.btnGoTo.ForeColor = System.Drawing.Color.White;
+            this.btnGoTo.Icon = null;
+            this.btnGoTo.IconScaling = 0.6F;
+            this.btnGoTo.Location = new System.Drawing.Point(565, 0);
+            this.btnGoTo.Name = "btnGoTo";
+            this.btnGoTo.Size = new System.Drawing.Size(35, 32);
+            this.btnGoTo.TabIndex = 2;
+            this.btnGoTo.Text = "➡︎";
+            this.btnGoTo.UsePureColorIcon = true;
+            this.btnGoTo.UseVisualStyleBackColor = true;
+            this.btnGoTo.Click += new System.EventHandler(this.OnGoToButtonClicked);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnBack.BorderColor = System.Drawing.Color.Empty;
+            this.btnBack.BorderSize = 1;
+            this.btnBack.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnBack.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(135)))), ((int)(((byte)(225)))));
+            this.btnBack.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(99)))), ((int)(((byte)(164)))));
+            this.btnBack.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(147)))), ((int)(((byte)(246)))));
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("等线", 15F);
+            this.btnBack.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnBack.Icon = null;
+            this.btnBack.IconScaling = 0.6F;
+            this.btnBack.Location = new System.Drawing.Point(0, 0);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(35, 32);
+            this.btnBack.TabIndex = 0;
+            this.btnBack.Text = "🔙";
+            this.btnBack.UsePureColorIcon = true;
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.OnBackButtonClicked);
+            // 
+            // cmsDrives
+            // 
+            this.cmsDrives.Name = "cmsDrives";
+            this.cmsDrives.Size = new System.Drawing.Size(61, 4);
+            // 
+            // cmsFileContextMenu
+            // 
+            this.cmsFileContextMenu.Name = "cmsFileContextMenu";
+            this.cmsFileContextMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // colName
+            // 
+            this.colName.Text = "Name";
+            this.colName.Width = 206;
+            // 
+            // colSize
+            // 
+            this.colSize.Text = "Size";
+            this.colSize.Width = 148;
+            // 
+            // colCteationTime
+            // 
+            this.colCteationTime.Text = "Time";
+            this.colCteationTime.Width = 131;
+            // 
+            // listView
+            // 
+            this.listView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colName,
+            this.colSize,
+            this.colCteationTime});
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.HideSelection = false;
+            this.listView.LargeImageList = this.imageList;
+            this.listView.Location = new System.Drawing.Point(0, 35);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(600, 365);
+            this.listView.SmallImageList = this.imageList;
+            this.listView.TabIndex = 2;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            this.listView.SelectedIndexChanged += new System.EventHandler(this.listView_SelectedIndexChanged);
+            this.listView.MouseClick += new System.Windows.Forms.MouseEventHandler(this.OnFileItemMouseClicked);
+            this.listView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.OnListViewMouseDoubleClicked);
+            // 
+            // DriveBrowserView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.panel1);
+            this.Name = "DriveBrowserView";
+            this.Size = new System.Drawing.Size(600, 400);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        protected SimpleButton btnBack;
+        protected ApeForms.Core.Controls.RoundTextPanel tbPath;
+        protected SimpleButton btnGoTo;
+        protected SimpleButton btnDrive;
+        protected System.Windows.Forms.ImageList imageList;
+        protected System.Windows.Forms.ContextMenuStrip cmsDrives;
+        protected System.Windows.Forms.ContextMenuStrip cmsFileContextMenu;
+        protected System.Windows.Forms.ColumnHeader colName;
+        protected System.Windows.Forms.ColumnHeader colSize;
+        protected System.Windows.Forms.ColumnHeader colCteationTime;
+        protected System.Windows.Forms.ListView listView;
+        protected SimpleButton btnPrevious;
+    }
+}
---
+++ ApeFree.ApeForms.Core/Controls/Views/DriveBrowserView.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ApeFree.ApeForms.Core.Controls.Views
+{
+    public partial class DriveBrowserView : UserControl
+    {
+        /// <summary>
+        /// 远程文件夹历史路径
+        /// </summary>
+        protected Stack<string> HistoryStack { get; } = new Stack<string>();
+
+        /// <summary>
+        /// 控件当前输入的远程文件夹路径
+        /// </summary>
+        public string CurrentPath => HistoryStack.Peek();
+
+        /// <summary>
+        /// 是否允许多选
+        /// </summary>
+        public bool MultiSelect { get => listView.MultiSelect; set => listView.MultiSelect = value; }
+
+        /// <summary>
+        /// 是否允许文件拖拽
+        /// </summary>
+        public override bool AllowDrop { get => listView.AllowDrop; set => listView.AllowDrop = value; }
+
+        /// <summary>
+        /// 显示项类型
+        /// </summary>
+        public DisplayItemType DisplayItemType { get; set; }
+
+        /// <summary>
+        /// 选中文件路径集合
+        /// </summary>
+        public string[] SelectedFiles { get; private set; } = new string[0];
+
+        /// <summary>
+        /// 选中文件夹路径集合
+        /// </summary>
+        public string[] SelectedFolders { get; private set; } = new string[0];
+
+        /// <summary>
+        /// 选中文件
+        /// </summary>
+        public string FilePath => SelectedFiles?.FirstOrDefault();
+
+        /// <summary>
+        /// 选中文件夹
+        /// </summary>
+        public string FolderPath => SelectedFolders?.FirstOrDefault();
+
+        /// <summary>
+        /// 与文件名匹配的搜索字符串
+        /// </summary>
+        public string SearchPattern { get; set; } = "*";
+
+        /// <summary>
+        /// 菜单项目被选中时出发此事件
+        /// </summary>
+
+        public event EventHandler OnSelectedItemsChanged;
+
+        public DriveBrowserView()
+        {
+            InitializeComponent();
+            AllowDrop = false;
+        }
+
+        private void listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var items = listView.SelectedItems.Cast<ListViewItem>();
+
+            SelectedFiles = items.Where(x => x.ImageKey == "File").Select(x => Path.Combine(CurrentPath, x.Text)).ToArray();
+            SelectedFolders = items.Where(x => x.ImageKey == "Folder").Select(x => Path.Combine(CurrentPath, x.Text)).ToArray();
+
+            OnSelectedItemsChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void OnPreviousButtonClicked(object sender, EventArgs e)
+        {
+            var path = CurrentPath;
+            var folder = new DirectoryInfo(path);
+            path = folder.Parent == null ? path : folder.Parent.FullName;
+
+            OpenFolder(path);
+        }
+
+        protected virtual void OnListViewMouseDoubleClicked(object sender, MouseEventArgs e)
+        {
+            ListViewHitTestInfo hitTest = listView.HitTest(e.X, e.Y);
+            if (hitTest.Item == null)
+            {
+                return;
+            }
+
+            var dir = HistoryStack.Peek();
+
+            if (hitTest.Item.Tag is DirectoryInfo di)
+            {
+                // 如果选中的时文件夹，就跳转进入该目录
+                OpenFolder(di.FullName);
+            }
+            else if (hitTest.Item.Tag is FileInfo fi)
+            {
+                // 如果选中的时文件
+                OnFileItemDoubleClicked(dir, fi, hitTest);
+            }
+        }
+
+        protected virtual void OnGoToButtonClicked(object sender, EventArgs e)
+        {
+            var path = tbPath.Text;
+            OpenFolder(path);
+        }
+
+        protected virtual void OnFileItemMouseClicked(object sender, MouseEventArgs e)
+        {
+            ListViewHitTestInfo hitTest = listView.HitTest(e.X, e.Y);
+            if (hitTest.Item == null)
+            {
+                return;
+            }
+
+            if (e.Button == MouseButtons.Right)
+            {
+                if (hitTest.Item.Tag is DirectoryInfo di)
+                {
+                    OpenFolderContextMenu(cmsFileContextMenu, di);
+                }
+                else if (hitTest.Item.Tag is FileInfo fi)
+                {
+                    OpenFileContextMenu(cmsFileContextMenu, fi);
+                }
+            }
+        }
+
+        protected virtual void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            if (HistoryStack.Count <= 1)
+            {
+                return;
+            }
+
+            HistoryStack.Pop();
+            var path = HistoryStack.Peek();
+            tbPath.Text = path;
+            OpenFolder(path);
+        }
+
+        protected virtual void OnDriveButtonClicked(object sender, EventArgs e)
+        {
+            var dis = DriveInfo.GetDrives();
+            cmsDrives.Items.Clear();
+
+            var arr = dis.Select(x => new ToolStripMenuItem(x.Name, null, (_, arg) => OpenFolder(x.RootDirectory.FullName))).ToArray();
+            cmsDrives.Items.AddRange(arr);
+
+            cmsDrives.Show(btnDrive, new Point(0, btnDrive.Height));
+        }
+
+        #region 文件拖拽
+        protected virtual void OnFileDragEnter(object sender, DragEventArgs e)
+        {
+            if (!AllowDrop)
+            {
+                return;
+            }
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        protected virtual void OnFileDragDrop(object sender, DragEventArgs e)
+        {
+            if (!AllowDrop)
+            {
+                return;
+            }
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                foreach (string file in files)
+                {
+                    var fileName = Path.GetFileName(file);
+                    var targetPath = Path.Combine(CurrentPath, fileName);
+                    File.Move(file, targetPath);
+                }
+                OpenFolder(CurrentPath);
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// 打开选中文件的快捷菜单
+        /// </summary>
+        /// <param name="contextItem"></param>
+        protected virtual void OpenFileContextMenu(ContextMenuStrip cmsMenu, object contextItem) { }
+
+        /// <summary>
+        /// 打开选文件夹的快捷菜单
+        /// </summary>
+        /// <param name="cmsMenu"></param>
+        /// <param name="di"></param>
+        protected virtual void OpenFolderContextMenu(ContextMenuStrip cmsMenu, object contextItem) { }
+
+        /// <summary>
+        /// 当文件项被鼠标双击
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <param name="item"></param>
+        /// <param name="hitTest"></param>
+        protected virtual void OnFileItemDoubleClicked(string dir, object contextItem, ListViewHitTestInfo hitTest) { }
+
+        /// <summary>
+        /// 打开文件夹
+        /// </summary>
+        /// <param name="path"></param>
+        public virtual void OpenFolder(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            var dirItems = directory.GetDirectories();
+            var fileItems = DisplayItemType == DisplayItemType.FolderAndFile ? directory.GetFiles(SearchPattern) : [];
+
+            var lvis = new List<ListViewItem>();
+
+            foreach (var item in dirItems)
+            {
+                ListViewItem lvi = new ListViewItem(item.Name);
+                lvi.ImageKey = "Folder";
+                lvi.Tag = item;
+                lvis.Add(lvi);
+            }
+
+            foreach (var item in fileItems)
+            {
+                ListViewItem lvi = new ListViewItem(item.Name);
+                lvi.SubItems.Add(item.Length.ToString());
+                lvi.SubItems.Add(item.CreationTime.ToString());
+                lvi.ImageKey = "File";
+                lvi.Tag = item;
+                lvis.Add(lvi);
+            }
+
+            listView.ModifyInUI(() =>
+            {
+                tbPath.Text = path;
+
+                if (!HistoryStack.Any() || HistoryStack.Peek() != path)
+                {
+                    HistoryStack.Push(path);
+                }
+
+                listView.Items.Clear();
+                if (lvis.Any())
+                {
+                    listView.Items.AddRange(lvis.ToArray());
+                }
+            });
+        }
+
+        private void tbPath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                OpenFolder(tbPath.Text);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// 显示项类型
+    /// </summary>
+    public enum DisplayItemType
+    {
+        /// <summary>
+        /// 文件夹和文件
+        /// </summary>
+        FolderAndFile,
+
+        /// <summary>
+        /// 仅文件夹
+        /// </summary>
+        OnlyFolder,
+    }
+}
